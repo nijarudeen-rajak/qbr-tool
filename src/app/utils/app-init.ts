73e421a7f096032b191d1394f4fe23cee1cea2bf
mukeshAnandan <mukesh@ideas2it.com>
2024-01-30 14:41:23 +0530

    Keycloak issue fix

--- src/app/utils/app-init.ts
+++ src/app/utils/app-init.ts
@@ -1,17 +1,31 @@
+// app-init.ts
+
 import { KeycloakService } from 'keycloak-angular';
 
+const reloadFlagKey = 'reloadFlag';
+
 export function initializer(keycloak: KeycloakService): () => Promise<any> {
-  return () =>
-    keycloak.init({
+  return () => {
+    // Check if the reload flag is set in localStorage
+    const reloadFlag = localStorage.getItem(reloadFlagKey);
+
+    // If the flag is not set and the user is not authenticated, perform the reload and set the flag
+    if (!reloadFlag) {
+      localStorage.setItem(reloadFlagKey, 'true');
+      window.location.reload();
+    }
+
+    return keycloak.init({
       config: 'assets/keycloak.json',
       initOptions: {
         onLoad: 'login-required',
-        checkLoginIframe: false,
+        checkLoginIframe: true,
       },
       loadUserProfileAtStartUp: false,
-      enableBearerInterceptor: true,
+      enableBearerInterceptor: false,
       bearerExcludedUrls: ['/assets', '/clients-public'],
     });
+  };
 }
 
 // export function initializer(keycloak: KeycloakService): () => Promise<any> {
